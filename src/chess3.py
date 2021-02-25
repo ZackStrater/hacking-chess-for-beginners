15384de60f42f222a986cfba9c1de8751882a229
Zack Strater <zackstrater@Zack.Strater.Lenovo>
2021-02-24 23:23:38 -0800

    starting heatmap

--- src/chess3.py
+++ src/chess3.py
@@ -1,14 +1,75 @@
 import chess
 import chess.engine
+import numpy as np
+print(np.version.version)
+
+chess_dict = {
+    'r': 7,
+    'n': 8,
+    'b': 9,
+    'k': 10,
+    'q': 11,
+    'p': 12,
+    'R': 1,
+    'N': 2,
+    'B': 3,
+    'K': 4,
+    'Q': 5,
+    'P': 6,
+    0: 0
+    }
+
+
+
+
+def convert_board_to_array(board_state):
+    piece_positions = {k:str(v) for k,v in board_state.piece_map().items()}
+
+    for i in range(64):
+        if i not in piece_positions:
+            piece_positions[i] = 0
+
+    board_pieces_list = [chess_dict[piece_positions[i]] for i in (range(64))]
+    board_array =  np.array(board_pieces_list).reshape(8,8)
+    return np.flip(board_array, axis=0)
+
+def isolate_move(current_move_array, previous_move_array):
+    difference_array = current_move_array - previous_move_array
+    isolated_move = difference_array[difference_array < 0] = 0
+    return isolated_move
+
+def compile_moves(moves):
+    board = chess.Board()
+    moves_bin = []
+    for i in range(moves):
+        board.push_san(moves[i])
+    # sum array to find out what piece was moved
+    # castling would count as rook + king
+
+
 
 
 board = chess.Board()
-engine = chess.engine.SimpleEngine.popen_uci("/home/zackstrater/stockfish_13_linux_x64_bmi2/stockfish_13_linux_x64_bmi2")
 
-def stockfish_evaluation(board, engine, depth = 20):
-    result = engine.analyse(board, chess.engine.Limit(depth=depth))
-    return result['score']
-for k,v in board.piece_map().items():
-    print(k,v)
 
-engine.quit()
+move_0 = convert_board_to_array(board)
+
+board.push_san('e4')
+
+move_1 = convert_board_to_array(board)
+white_1 = move_1-move_0
+white_1[white_1<0] = 0
+print(white_1)
+
+board.push_san('e5')
+
+move_2 = convert_board_to_array(board)
+print(move_2-move_1)
+black_1[black_1>0] = 0
+print(white_1)
+
+board.push_san('d4')
+
+move_3 = convert_board_to_array(board)
+print(move_3-move_2)
+
--- src/plotting.py
+++ src/plotting.py
@@ -117,7 +117,6 @@ y = gp2['play_percent']
 x = gp2['result']
 fig, ax = plt.subplots()
 ax.scatter(x, y, alpha=0.3, s=20)
-ax.set_ylim(-0.05, 4)
 ax.set_xlabel("white win percent")
 ax.set_ylabel("play percent")
 plt.show()
@@ -141,7 +140,80 @@ y = gp2['play_percent']
 x = gp2['result']
 fig, ax = plt.subplots()
 ax.scatter(x, y, alpha=0.3, s=20)
-ax.set_ylim(-0.05, 4)
 ax.set_xlabel("white win percent")
 ax.set_ylabel("play percent")
+plt.show()
+
+
+
+
+
+
+groups2 = df4.groupby('1w').mean().reset_index()
+groups3 = df4.groupby('1w').count().reset_index()
+gp2 = groups2.sort_values('1w')
+gp3 = groups3.sort_values('1w')
+
+gp2['play_percent'] = gp3['game_id']/3929420*100
+gp2.sort_values('result', ascending = False, inplace=True)
+gp2.head(10)
+
+
+y = gp2['play_percent']
+x = gp2['result']
+fig, ax = plt.subplots()
+ax.scatter(x, y, alpha=0.3, s=20)
+ax.set_ylim(-0.05, 4)
+ax.set_xlabel("white win %")
+ax.set_ylabel("play %")
+plt.show()
+
+
+y = gp2['play_percent']
+x = gp2['result']
+fig, ax = plt.subplots(figsize=(10, 5))
+ax.scatter(x, y, alpha=1, s=20)
+ax.set_ylim(-10, 70)
+ax.set_xlim(0.42, 0.55)
+ax.set_xlabel("white win %")
+ax.set_ylabel("play %")
+ax.annotate(gp2['1w'][0], (x[0], y[0]), textcoords="offset points", xytext=(-10, 10))
+ax.annotate(gp2['1w'][1], (x[1], y[1]), textcoords="offset points", xytext=(0, 10))
+ax.annotate(gp2['1w'][2], (x[2], y[2]), textcoords="offset points", xytext=(0, 10))
+ax.annotate(gp2['1w'][3], (x[3], y[3]), textcoords="offset points", xytext=(0, 10))
+ax.annotate(gp2['1w'][4], (x[4], y[4]), textcoords="offset points", xytext=(-15, -15))
+ax.annotate(gp2['1w'][5], (x[5], y[5]), textcoords="offset points", xytext=(-15, -15))
+ax.annotate(gp2['1w'][6], (x[6], y[6]), textcoords="offset points", xytext=(10, 0))
+ax.annotate(gp2['1w'][7], (x[7], y[7]), textcoords="offset points", xytext=(5, 5))
+ax.annotate(gp2['1w'][8], (x[8], y[8]), textcoords="offset points", xytext=(10, 0))
+ax.annotate(gp2['1w'][9], (x[9], y[9]), textcoords="offset points", xytext=(10, 10))
+ax.annotate(gp2['1w'][10], (x[10], y[10]), textcoords="offset points", xytext=(-10, 10))
+ax.annotate(gp2['1w'][11], (x[11], y[11]), textcoords="offset points", xytext=(10, 0))
+ax.annotate(gp2['1w'][12], (x[12], y[12]), textcoords="offset points", xytext=(0, 10))
+ax.annotate(gp2['1w'][13], (x[13], y[13]), textcoords="offset points", xytext=(10, 0))
+ax.annotate(gp2['1w'][14], (x[14], y[14]), textcoords="offset points", xytext=(10, 0))
+ax.annotate(gp2['1w'][15], (x[15], y[15]), textcoords="offset points", xytext=(0, 10))
+ax.annotate(gp2['1w'][16], (x[16], y[16]), textcoords="offset points", xytext=(-15, -15))
+ax.annotate(gp2['1w'][17], (x[17], y[17]), textcoords="offset points", xytext=(-15, -15))
+ax.annotate(gp2['1w'][18], (x[18], y[18]), textcoords="offset points", xytext=(10, 0))
+ax.annotate(gp2['1w'][19], (x[19], y[19]), textcoords="offset points", xytext=(0, 10))
+plt.show()
+
+
+
+
+y = gp2['play_percent']
+x = gp2['result']
+fig, ax = plt.subplots(figsize=(10, 5))
+ax.scatter(x, y, alpha=1, s=20)
+ax.set_ylim(-9, 55)
+ax.set_xlim(0.49, 0.55)
+ax.set_xlabel("white win %")
+ax.set_ylabel("play %")
+for i, txt in enumerate(gp2['1b']):
+    if i not in [2, 11, 19]:
+        ax.annotate(txt, (x[i], y[i]), textcoords="offset points", xytext=(0, 10))
+ax.annotate(gp2['1b'][2], (x[2], y[2]), textcoords="offset points", xytext=(0, 10))
+ax.annotate(gp2['1b'][11], (x[11], y[11]), textcoords="offset points", xytext=(5, -10))
+ax.annotate(gp2['1b'][19], (x[19], y[19]), textcoords="offset points", xytext=(-20, 0))
 plt.show()
